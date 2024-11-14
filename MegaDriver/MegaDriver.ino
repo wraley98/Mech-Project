44962ffdb12948ede1fcbb842781817599017938
William Raley <williamraley@Williams-MacBook-Pro.local>
2024-11-13 23:45:26 -0700

    Added comments to code

--- MegaDriver/MegaDriver.ino
+++ MegaDriver/MegaDriver.ino
@@ -131,106 +131,157 @@ void loop() {
 
   // If activation signal is sent from uno, startDriving
   if (active.equals("1")) {
-    switch (direction) {
-      case 1:
 
+    // direction (1 = forward // 0 = backward)
+    switch (direction) {
+
+        // if robot is going forward
+      case 1:
+        // if robot is not going to the refinery
         if (!(goingToRefinery)) {
-          Serial.println("looking for intersection");
+          // if the robot is going to the safe zone
           if (safeZone) {
+            // check for an intersection
             CheckIntersection();
           } else {
+            // check for wall
             CheckWall();
+            // grab the block if the robot is at the wall
             if (atWall)
               GrabBlock();
           }
         } else {
+          // look for refinery wall
           CheckWall();
+          // if the refinery has been reached
           if (atRefinery)
+            // dispense the block
             DispenseBlock();
         }
 
+        // follow the line
         LineFollow();
 
         break;
 
+      // if the robot is moving backwards
       case 0:
+        // move the robot backwards
         mdWheels.setSpeeds(100, -100);
+        // look for the line
         CheckIntersection();
+        break;
     }
   }
 }
 
 // Drive Functions
 
+/*
+LineFollow
+
+Retrieves line sensor values and updates the robots 
+motors to allign the bot with the line. 
+*/
 void LineFollow(void) {
 
-  double denom = 0;
-  double d = 0;
-  double error = 0;
-  double num = 0;
+  double denom = 0;  // denominator for calculating distance from line
+  double num = 0;    // numinator for calculating distance from line
+  double d = 0;      // distance from center of line
+  double error = 0;  // error between center of bot and center of line
 
+  // retrieve unbiased sensor values
   GetUnbiased();
 
+  // if the robot is going backwards, return
   if (direction == 0)
     return;
 
+  // add up the numerator and denomitor values
   for (uint8_t i = 0; i < 8; i++) {
     num += Sensor_value_unbiased[i] * sensor_loc[i];
     denom += Sensor_value_unbiased[i];
   }
 
+  // location of the center line on the sensors
   d = num / denom;
 
+  // error between the location of the center line and center robot
   error = 2 - d;
 
-  // put your line following code here
+  // motor adjustments based on the error
   m1c = base_speed + kpLineSensor * error;
   m2c = base_speed - kpLineSensor * error;
 
-  mdWheels.setSpeeds(-m1c, m2c);  // send motor commands
+  // send motor commands
+  mdWheels.setSpeeds(-m1c, m2c);
 }
+/*
+CheckIntersection
 
+Looks for intersections along the safe zone line 
+or just the safe zone line.
+*/
 void CheckIntersection(void) {
 
-  double sum = 0;
-  int numSensors;
-  double avg = 0;
+  double sum = 0;  // initializes the sum of the sensor values
+  int numSensors;  // initializes the number of sensors that will be used
+  double avg = 0;  // initializes the average variable
 
+  // retrieve the unbiased sensor values
   GetUnbiased();
 
+  // if the robot is going forward, use the left half of the sensors
   if (direction == 1)
     numSensors = 5;
+  // if the robot is going backwards, use all sensors
   else
     numSensors = 8;
 
+  // add up all the unbiased sensors
   for (uint8_t i = 0; i < numSensors; i++)
     sum += Sensor_value_unbiased[i];
 
+  // calculate the average of the sensor values
   avg = sum / numSensors;
 
+  // if the robot is going backward
   if (direction == 0) {
+
+    // if the sensor average is greater than 1000
     if (avg > 1000) {
-
+      // stop the robot
       mdWheels.setSpeeds(0, 0);
+      // reset the current intersection
       currIntersection = 0;
+      // turn the robot
       Turn();
-
+      // set the robot going forward
       direction = 1;
+      // set the robot to going to the refinery
       goingToRefinery = true;
     }
   } else {
+    // if the sensor average is greater than 1200
     if (avg > 1200) {
+      // if the robot is not on the intersection with blocks
       if (currIntersection != activeIntersection) {
+        // if the robot is currently on an intersection, return
         if (onIntersection) {
           return;
-        } else {
+        }
+        // set onIntersection to true and return
+        else {
           onIntersection = true;
           return;
         }
       } else {
+        // turn the robot
         Turn();
       }
     } else {
+      // if the robot is on an intersection, increment the intersection
+      // tracker and set the onIntersection to false
       if (onIntersection) {
         currIntersection += 1;
         onIntersection = false;
@@ -239,18 +290,31 @@ void CheckIntersection(void) {
   }
 }
 
+/*
+GetUnbiased
+
+Retrieves the sensor values and calculates the 
+the unbiased sensor values
+*/
 void GetUnbiased(void) {
 
-
+  // retrieve sensor values
   qtr.read(sensorValues);
 
+  // calculate the unbiased sensor values
   for (uint8_t i = 0; i < 8; i++) {
     Sensor_value_unbiased[i] = sensorValues[i] - sensor_bias[i];
   }
 }
+/*
+Turn
 
+Turns the robot based on what intersection the 
+robot is currently on. 
+*/
 void Turn(void) {
 
+  // let the robot drive for .25 seconds
   delay(250);
 
   // stops bot
@@ -284,33 +348,42 @@ void Turn(void) {
   // Motor voltage commands
   double V1m, V2m;
 
+  // Set the time values
   t_old = micros() / 1000000.;
   t0 = micros() / 1000000.;
 
-  // turn
-  float angle;
+  float angle;  // initializes the angle that robot is turning to
 
+  // determines the angle to turn by based
+  // on the current intersection the bot is on
   switch (currIntersection) {
+    // robot is leaving the refinery
     case (-1):
       angle = M_PI / 1.2;
       break;
+    // robot is driving backwards from danger zone
     case (0):
+      // continue driving backwards for 0.3 seconds
       mdWheels.setSpeeds(-75, 75);
       delay(300);
       mdWheels.setSpeeds(0, 0);
       angle = M_PI / 4.;
       break;
+    // first intersection
     case (1):
       angle = M_PI / 5.3;
       break;
+    // second intersection
     case (2):
       angle = M_PI / 10.;
       break;
+    // third intersection
     case (3):
       angle = M_PI / 12.;
       break;
   };
 
+  // initializes the final angle
   theta1_final = angle * (D / 2) / rw;
   theta2_final = -angle * (D / 2) / rw;
 
@@ -318,9 +391,11 @@ void Turn(void) {
   omega1_des = 2;
   omega2_des = -omega1_des;
 
+  // reset the encoders
   myEnc1.write(0);
   myEnc2.write(0);
 
+  // infinite loop
   while (true) {
 
     t = micros() / 1000000. - t0;  // Current time in seconds since start
@@ -330,9 +405,12 @@ void Turn(void) {
     counts1 = myEnc1.read();
     counts2 = -myEnc2.read();
 
+
+    // update the angle of the wheels
     theta1 = -counts1 * 360 / (countsPerRev * GearRatio) * (M_PI / 180);
     theta2 = -counts2 * 360 / (countsPerRev * GearRatio) * (M_PI / 180);
 
+    // set the angular speed
     omega1 = (theta1 - theta1_old) / deltaT;
     omega2 = (theta2 - theta2_old) / deltaT;
 
@@ -349,6 +427,7 @@ void Turn(void) {
     V1m += correction;
     V2m -= correction;
 
+    // if either angles are greater than the final angle, stop
     if (abs(theta1) >= abs(theta1_final) || abs(theta2) >= abs(theta2_final)) {
       break;
     }
@@ -368,9 +447,11 @@ void Turn(void) {
     theta2_old = theta2;
   }
 
+  // if the robot is not driving backwards or leaving the refinery,
+  // the bot is in the danger zone
   if (currIntersection != 0 || currIntersection != -1)
     safeZone = false;
-
+  // else, the bot is in the safe zone
   else
     safeZone = true;
 
@@ -379,26 +460,39 @@ void Turn(void) {
 
 // Sensor Functions
 
+/*
+CheckWall
+
+Determines if the robot is close to a wall.
+*/
 void CheckWall(void) {
 
+  // if the distance sensor reading is too high, ignore the reading
   if (analogRead(distSensor) > 200)
     return;
 
+  // if the reading is greater than 180, stop
   if (analogRead(distSensor) > 180) {
+   
+    // if the robot is going to the refinery,
+    // set at reinery to true
     if (goingToRefinery) {
       mdWheels.setSpeeds(0, 0);
       atRefinery = true;
       return;
     }
-
+    
+    // stop the robot
     mdWheels.setSpeeds(0, 0);
+    // set the robot at the wall is true
     atWall = true;
-    approach = false;
 
+    // grab the block
     GrabBlock();
   }
 }
 
+/// !!!! Need to Implement!!!!
 void CalcWormRate(void) {
 
 
@@ -429,6 +523,7 @@ void CalcWormRate(void) {
 
 // Sand dune Functions
 
+/// !!!! Need to Implement !!!!
 void GrabBlock(void) {
 
   delay(1000);
@@ -450,6 +545,7 @@ void GrabBlock(void) {
   // armServo.write(0);
 }
 
+/// !!!! Need to Implement !!!!
 int CheckBlock(void) {
 
   if (abs(analogRead(hallSensor)) > 100)
@@ -458,6 +554,7 @@ int CheckBlock(void) {
   // need to add check color
 }
 
+/// !!!! Need to Implement !!!!
 void PushBlock(void) {
   // rotate arm up
   // armServo.write(90);
@@ -476,6 +573,7 @@ void PushBlock(void) {
   // mdRP.setM1Speed(400);
 }
 
+/// !!!! Need to Implement !!!!
 void DispenseBlock(void) {
 
   mdWheels.setSpeeds(0, 0);
@@ -496,5 +594,6 @@ void DispenseBlock(void) {
   atRefinery = false;
 }
 
+/// !!!! Need to Implement !!!!
 void CheckColor(void) {
 }
